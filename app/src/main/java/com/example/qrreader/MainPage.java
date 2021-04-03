30eb7c4eac564f8e56288697708353eaa16cfefb
Nidula Chithwara <chithwara@gmail.com>
2021-04-03 20:01:54 +0530

    QR Scanner UI Development

--- app/src/main/java/com/example/qrreader/MainPage.java
+++ app/src/main/java/com/example/qrreader/MainPage.java
@@ -2,13 +2,30 @@ package com.example.qrreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainPage extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        button=(Button) findViewById(R.id.button7);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity();
+            }
+        });
+    }
+    public void openActivity(){
+        Intent intent = new Intent(this, QrScan.class);
+        startActivity(intent);
     }
 }
